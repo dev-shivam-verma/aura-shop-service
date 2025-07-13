8edfc5f161272cdbee33174ef47bcfb2a2b07569
Shivam <shivamvrma3012@gmail.com>
2025-07-13 11:19:00 +0530

    initial commit

---
+++ src/main/java/com/aurashop/aurashopservice/AuraShopServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aurashop.aurashopservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraShopServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuraShopServiceApplication.class, args);
+    }
+
+}
